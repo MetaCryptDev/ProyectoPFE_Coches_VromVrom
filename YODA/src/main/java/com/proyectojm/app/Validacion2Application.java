571b1c9ffc69c1ed5ebb644715f3ba4ca17421ce
Moowette <108137567+jdelgadofernandez@users.noreply.github.com>
2024-03-06 19:02:29 +0100

    Add files via upload

---
+++ YODA/src/main/java/com/proyectojm/app/Validacion2Application.java
@@ -0,0 +1,13 @@
+package com.proyectojm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Validacion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Validacion2Application.class, args);
+	}
+
+}
