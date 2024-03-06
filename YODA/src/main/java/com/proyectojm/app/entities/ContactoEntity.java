1fef3ae21a847901e1e30810838248501bb34166
Moowette <108137567+jdelgadofernandez@users.noreply.github.com>
2024-03-06 19:30:14 +0100

    Add files via upload

---
+++ YODA/src/main/java/com/proyectojm/app/entities/ContactoEntity.java
@@ -0,0 +1,108 @@
+package com.proyectojm.app.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "CONTACTO")
+public class ContactoEntity implements Serializable{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "ID_CONTACTO")
+	private Integer id;
+	
+	@Id
+	@Column(name = "NOMBRE")
+	private String nombre;
+	
+	@Id
+	@Column(name = "APELLIDOS")
+	private String apellidos;
+	
+	@Id
+	@Column(name = "MAIL")
+	private String mail;
+	
+	@Id
+	@Column(name = "CONSULTA")
+	private String consulta;
+	
+	@Id
+	@Column(name = "CONTESTADO")
+	private Integer contestado;
+
+	public ContactoEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ContactoEntity(Integer id, String nombre, String apellidos, String mail, String consulta,
+			Integer contestado) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.mail = mail;
+		this.consulta = consulta;
+		this.contestado = contestado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getConsulta() {
+		return consulta;
+	}
+
+	public void setConsulta(String consulta) {
+		this.consulta = consulta;
+	}
+
+	public Integer getContestado() {
+		return contestado;
+	}
+
+	public void setContestado(Integer contestado) {
+		this.contestado = contestado;
+	}
+	
+}
---
+++ YODA/src/main/java/com/proyectojm/app/entities/PiezaEntity.java
@@ -0,0 +1,14 @@
+package com.proyectojm.app.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="PIEZA")
+public class PiezaEntity implements Serializable{
+
+	
+	
+}
