ebb5a4b15f02b730eae8d0118d337018e9c43b0a
Moowette <108137567+jdelgadofernandez@users.noreply.github.com>
2024-03-06 19:34:49 +0100

    cambios hiiii

--- YODA/src/main/java/com/proyectojm/app/entities/RolEntity.java
+++ YODA/src/main/java/com/proyectojm/app/entities/RolEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-//holi
+//holi hiiiiiii
 @Entity
 @Table(name="ROL")
 public class RolEntity implements Serializable{
