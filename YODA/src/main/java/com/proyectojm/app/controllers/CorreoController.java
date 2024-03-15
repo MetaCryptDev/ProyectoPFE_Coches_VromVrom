4df9d37a572d92c41854f3d16d20887deeb7df2c
miriamdelolmo <148791098+MiriamdelOlmo2023@users.noreply.github.com>
2024-03-15 10:30:18 +0100

    metodos correo registro + contacto

--- YODA/src/main/java/com/proyectojm/app/controllers/CorreoController.java
+++ YODA/src/main/java/com/proyectojm/app/controllers/CorreoController.java
@@ -31,4 +31,42 @@ public class CorreoController {
             e.printStackTrace();
         }
     }
+    
+    public void registro(String nombre, String email) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+
+        try {
+            helper.setTo(email);
+            helper.setSubject("Bienvenido a Talleres YoDa");
+            helper.setText("¡Hola, " + nombre + "! \n"
+            		+ "Tu cuenta ha sido registrada correctamente, ya estás listo para pedir cita cuando lo necesites y aprovechar nuestras ofertas. \n"
+            		+ "¡Gracias por elegir nuestro taller!");
+
+            javaMailSender.send(mimeMessage);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void correoContacto(String mensaje, String email, String motivo, String nombre) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+
+        try {
+            helper.setTo(email);
+            helper.setSubject("Consulta: " + motivo + ". Talleres YoDa");
+            helper.setText("¡Hola, " + nombre + "! \n"
+            		+ "Gracias por ponerte en contacto con nosotros, hemos recibido la siguiente consulta: \n" 
+            		+ motivo + "\n" 
+            		+ mensaje + "\n"
+            		+ "Intentaremos resolver tu consulta a la mayor brevedad posible.");
+
+            javaMailSender.send(mimeMessage);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 }
