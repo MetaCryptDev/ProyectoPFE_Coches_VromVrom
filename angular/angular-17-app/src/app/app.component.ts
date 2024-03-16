0080dd0828b1d24ac4d4bd30031acef0710d2fd7
AMMartinez <am291092@gmail.com>
2024-03-16 23:57:53 +0100

    arreglado error que no se veia el login

--- angular/angular-17-app/src/app/app.component.ts
+++ angular/angular-17-app/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { IndexComponent } from './components/index/index.component';
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink,IndexComponent],
+  imports: [RouterOutlet, RouterLink,IndexComponent,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['../assets/css/custom.css','../assets/css/templatemo.css','../assets/css/fontawesome.css','../assets/css/fontawesome.min.css'],
 })
--- angular/angular-17-app/src/app/components/index/index.component.ts
+++ angular/angular-17-app/src/app/components/index/index.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent,HttpClientModule],
   templateUrl: './index.component.html',
   styleUrls: ['../../../assets/css/custom.css','../../../assets/css/templatemo.css','../../../assets/css/fontawesome.css','../../../assets/css/fontawesome.min.css']
 })
