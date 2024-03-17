55a5577f45816b418aa864fe5e2e897a41a317a2
AMMartinez <am291092@gmail.com>
2024-03-17 12:21:34 +0100

    Creado controller contacto

---
+++ YODA/src/main/java/com/proyectojm/app/controllers/ContactoController.java
@@ -0,0 +1,41 @@
+package com.proyectojm.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyectojm.app.dto.ContactoDto;
+import com.proyectojm.app.service.IServiceContacto;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
+@RestController
+@RequestMapping(value = "/usuarios")
+public class ContactoController {
+	
+	@Autowired
+	private IServiceContacto contactoService;
+	@Autowired
+	private CorreoController correos;
+	
+	@GetMapping(value = "/verTodos")
+	
+	
+	@PostMapping(value = "/insertar")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public void insertar(@RequestBody ContactoDto contacto) {
+		try {
+		
+			contactoService.guardarContacto(contacto);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+}
--- YODA/src/main/java/com/proyectojm/app/service/impl/ContactoServiceImpl.java
+++ YODA/src/main/java/com/proyectojm/app/service/impl/ContactoServiceImpl.java
@@ -2,11 +2,30 @@ package com.proyectojm.app.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.proyectojm.app.dao.IDAOContacto;
 import com.proyectojm.app.dto.ContactoDto;
 import com.proyectojm.app.dto.UsuarioDto;
+import com.proyectojm.app.entities.ContactoEntity;
 
 public class ContactoServiceImpl {
+	
+	@Autowired IDAOContacto contactoDao;
+	
 	public void guardarContacto(ContactoDto contacto) {
+		ContactoEntity contactoEntty=  new ContactoEntity();
+		try {
+			contactoEntty.setMail(contacto.getMail());
+			contactoEntty.setMensaje(contacto.getMensaje());
+			contactoEntty.setMotivo(contacto.getMotivo());
+			contactoEntty.setNombre(contacto.getNombre());
+			contactoDao.save(contactoEntty);
+			System.out.println("hecho");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
 		
 	}
 	public void modificarContacto(ContactoDto contacto) {
