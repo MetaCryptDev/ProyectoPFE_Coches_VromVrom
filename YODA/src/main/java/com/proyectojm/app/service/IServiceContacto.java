aa17f6215ec92009b9b2473e454b6b0c347cb476
miriamdelolmo <148791098+MiriamdelOlmo2023@users.noreply.github.com>
2024-03-17 13:04:53 +0100

    contacto servicio metodos

--- YODA/src/main/java/com/proyectojm/app/service/IServiceContacto.java
+++ YODA/src/main/java/com/proyectojm/app/service/IServiceContacto.java
@@ -3,14 +3,13 @@ package com.proyectojm.app.service;
 import java.util.List;
 
 import com.proyectojm.app.dto.ContactoDto;
-import com.proyectojm.app.dto.UsuarioDto;
 
 public interface IServiceContacto {
 
 	
 	public void guardarContacto(ContactoDto contacto);
 	public void modificarContacto(ContactoDto contacto);
-	public List<UsuarioDto> recuperarTodosLosContactos();
-	public UsuarioDto recuperarUnContacto(Integer id);
+	public List<ContactoDto> recuperarTodosLosContactos();
+	public ContactoDto recuperarUnContacto(Integer id);
 	
 }
--- YODA/src/main/java/com/proyectojm/app/service/impl/ContactoServiceImpl.java
+++ YODA/src/main/java/com/proyectojm/app/service/impl/ContactoServiceImpl.java
@@ -1,13 +1,15 @@
 package com.proyectojm.app.service.impl;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.proyectojm.app.dao.IDAOContacto;
 import com.proyectojm.app.dto.ContactoDto;
-import com.proyectojm.app.dto.UsuarioDto;
 import com.proyectojm.app.entities.ContactoEntity;
 import com.proyectojm.app.service.IServiceContacto;
 
@@ -16,9 +18,12 @@ public class ContactoServiceImpl implements IServiceContacto {
 	
 	@Autowired IDAOContacto contactoDao;
 	
+	@Override
+	@Transactional
 	public void guardarContacto(ContactoDto contacto) {
 		ContactoEntity contactoEntty=  new ContactoEntity();
 		try {
+			contactoEntty.setId(contacto.getId());
 			contactoEntty.setMail(contacto.getMail());
 			contactoEntty.setMensaje(contacto.getMensaje());
 			contactoEntty.setMotivo(contacto.getMotivo());
@@ -28,19 +33,76 @@ public class ContactoServiceImpl implements IServiceContacto {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
-		
 	}
+	
+	@Override
+	@Transactional
 	public void modificarContacto(ContactoDto contacto) {
-		
+		try {
+        	ContactoEntity contactoEntty = contactoDao.findById(contacto.getId()).orElse(null);
+
+            if (contactoEntty != null) {
+            	contactoEntty.setMail(contacto.getMail());
+    			contactoEntty.setMensaje(contacto.getMensaje());
+    			contactoEntty.setMotivo(contacto.getMotivo());
+    			contactoEntty.setNombre(contacto.getNombre());
+    			
+    			contactoDao.save(contactoEntty);
+            } else {
+                System.out.println("La entidad no existe.");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 	}
-	public List<UsuarioDto> recuperarTodosLosContactos() {
-		
-		return null;
-		
+	
+	@Override
+	@Transactional
+	public List<ContactoDto> recuperarTodosLosContactos() {
+			List<ContactoDto> lstResultado = null;
+	        Iterable<ContactoEntity> lstEntidades = null;
+
+	        try {
+	            lstResultado = new ArrayList<>();
+	            lstEntidades = contactoDao.findAll();
+
+	            for (Iterator<ContactoEntity> iterator = lstEntidades.iterator(); iterator.hasNext();) {
+	            	ContactoEntity entity = iterator.next();
+	            	ContactoDto actual = new ContactoDto();
+	            	
+	            	actual.setId(entity.getId());
+	            	actual.setMail(entity.getMail());
+	            	actual.setMensaje(entity.getMensaje());
+	            	actual.setMotivo(entity.getMotivo());
+	            	actual.setNombre(entity.getNombre());
+	            	
+	                lstResultado.add(actual);
+	            }
+
+	        } catch (Exception e) {
+	            System.out.println(e.getMessage());
+	        }
+
+	        return lstResultado;
 	}
-	public UsuarioDto recuperarUnContacto(Integer id) {
-		
-		return null;
+	
+	@Override
+	@Transactional
+	public ContactoDto recuperarUnContacto(Integer id) {
+		ContactoDto actual = new ContactoDto();
+		 ContactoEntity entity = null;
+      try {
+          entity = contactoDao.findById(id).orElse(null);
+          if (entity != null) {
+        	  actual.setId(entity.getId());
+	          	actual.setMail(entity.getMail());
+	          	actual.setMensaje(entity.getMensaje());
+	          	actual.setMotivo(entity.getMotivo());
+	          	actual.setNombre(entity.getNombre());
+          }
+      } catch (Exception e) {
+          System.out.println(e.getMessage());
+      }
+	        return actual;
 	}
 }
