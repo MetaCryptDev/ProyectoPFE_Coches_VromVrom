5c88f35832181cdd6418f6dc2c8395f6d89cf929
Moowette <108137567+jdelgadofernandez@users.noreply.github.com>
2024-03-18 09:12:40 +0100

    creacion cita

--- angular/angular-17-app/src/app/app.routes.ts
+++ angular/angular-17-app/src/app/app.routes.ts
@@ -3,12 +3,18 @@ import { app } from '../../server';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { IndexComponent } from './components/index/index.component';
+import { CitaComponent } from './components/cita/cita.component';
 
 export const routes: Routes = [{
     path:"", title:"Talleres YoDa", component: IndexComponent
 },
 {
     path:"login",title:"Registro", component: LoginComponent
+},
+
+{
+path:"cita",title:"Pedir Cita", component: CitaComponent
 }
+
 
 ];
