90555221722af97c32a4df9e63343f4c153ed852
miriamdelolmo <148791098+MiriamdelOlmo2023@users.noreply.github.com>
2024-03-20 19:56:00 +0100

    vehiculo sust form

--- angular/angular-17-app/src/app/components/cita/cita.component.ts
+++ angular/angular-17-app/src/app/components/cita/cita.component.ts
@@ -1,5 +1,4 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { Contacto } from '../../dto/contacto';
 import { ContactoService } from '../../servicios/contacto-service.service';
@@ -10,62 +9,68 @@ import { HttpClientModule } from '@angular/common/http';
 import { Cita } from '../../dto/cita';
 import { Usuario } from '../../dto/usuario';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-cita',
   standalone: true,
-  imports: [FooterComponent,FormsModule,RouterModule,CommonModule],
+  imports: [FooterComponent, FormsModule, RouterModule, CommonModule],
   templateUrl: './cita.component.html',
-  styleUrls: ['./cita.component.css','../../../assets/css/custom.css','../../../assets/css/templatemo.css','../../../assets/css/fontawesome.css','../../../assets/css/fontawesome.min.css']
-
+  styleUrls: ['./cita.component.css', '../../../assets/css/custom.css', '../../../assets/css/templatemo.css', '../../../assets/css/fontawesome.css', '../../../assets/css/fontawesome.min.css']
 })
 export class CitaComponent implements OnInit {
-  title : string = 'Nuevo Cita';
+  title: string = 'Nuevo Cita';
   horasDisponibles: string[] = [];
-  user:Usuario =new Usuario();
-  contacto : Contacto = new Contacto();
-  cita : Cita = new Cita();
+  user: Usuario = new Usuario();
+  contacto: Contacto = new Contacto();
+  cita: Cita = new Cita();
+  vehiculoSustBool: boolean = false;
 
-    constructor(private citaService : CitaService,
-                private router : Router,
-                private activatedRouter : ActivatedRoute) {}
+  constructor(private citaService: CitaService,
+    private router: Router,
+    private activatedRouter: ActivatedRoute,
+    private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     const currentUser = localStorage.getItem('currentUser');
-  if (currentUser) {
-    let userData = JSON.parse(currentUser);
-    this.contacto.nombre = userData.nombre;
-    this.contacto.mail = userData.mail;
-    this.cita.idUsuario = userData.idUsuario;
-  }
-  }
-
-              
-    crearCita() : void {
-      
-      this.cita.entrada = `${this.cita.fecha}T${this.cita.hora}`;
-      console.log('Enviando cita:', JSON.stringify(this.cita));
-      this.citaService.saveCita(this.cita).subscribe(
-        response => {
-          this.router.navigate(['']); 
-            
-        }
-      );
+    if (currentUser) {
+      let userData = JSON.parse(currentUser);
+      this.contacto.nombre = userData.nombre;
+      this.contacto.mail = userData.mail;
+      this.cita.idUsuario = userData.idUsuario;
     }
-
-    actualizarHorasDisponibles() {
-      if (this.cita.idServicio && this.cita.fecha) {
-          this.citaService.getHorasDisponibles(this.cita.idServicio, this.cita.fecha)
-              .subscribe(
-                  horas => {
-                      this.horasDisponibles = horas;
-                  },
-                  error => {
-                      console.error('Error al recuperar las horas disponibles:', error);
-                      this.horasDisponibles = []; // Limpiar las horas si hay un error
-                  }
-              );
-      }
   }
 
-}
+  toggleVehiculoSustitucion() {
+    this.vehiculoSustBool = !this.vehiculoSustBool;
+    const vehiculoSustDiv = document.getElementById("vehiculoSustDiv");
+    if (vehiculoSustDiv) {
+      vehiculoSustDiv.style.display = this.vehiculoSustBool ? "block" : "none";
+    }
+  }
+  
 
+  crearCita(): void {
+    this.cita.entrada = `${this.cita.fecha}T${this.cita.hora}`;
+    console.log('Enviando cita:', JSON.stringify(this.cita));
+    this.citaService.saveCita(this.cita).subscribe(
+      response => {
+        this.router.navigate(['']);
+      }
+    );
+  }
+
+  actualizarHorasDisponibles() {
+    if (this.cita.idServicio && this.cita.fecha) {
+      this.citaService.getHorasDisponibles(this.cita.idServicio, this.cita.fecha)
+        .subscribe(
+          horas => {
+            this.horasDisponibles = horas;
+          },
+          error => {
+            console.error('Error al recuperar las horas disponibles:', error);
+            this.horasDisponibles = []; // Limpiar las horas si hay un error
+          }
+        );
+    }
+  }
+}
