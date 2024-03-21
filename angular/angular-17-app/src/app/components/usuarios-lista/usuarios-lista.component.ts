f5b7205fc3f80f849da86ed461029ac3e9821c26
Moowette <108137567+jdelgadofernandez@users.noreply.github.com>
2024-03-21 17:54:53 +0100

    componentes usuarios

---
+++ angular/angular-17-app/src/app/components/usuarios-lista/usuarios-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuarios-lista.component.html',
+  styleUrl: './usuarios-lista.component.css'
+})
+export class UsuariosListaComponent {
+
+}
