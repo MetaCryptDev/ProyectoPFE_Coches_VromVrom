61f37671d8493832f335a0042eea27b230804dd8
Moowette <108137567+jdelgadofernandez@users.noreply.github.com>
2024-03-21 17:28:24 +0100

    tocando inventario insercion

--- YODA/src/main/java/com/proyectojm/app/controllers/InventarioController.java
+++ YODA/src/main/java/com/proyectojm/app/controllers/InventarioController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,6 +25,16 @@ public class InventarioController {
 	@Autowired
 	private IServiceInventario inventarioService;
 	
+	
+	@PutMapping(value = "/actualizar")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public void actualizar(@RequestBody InventarioDto inventario) {
+		try {
+			inventarioService.modificarPieza(inventario);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 		
 	@GetMapping(value = "/verTodos")
 	@ResponseStatus(value = HttpStatus.CREATED)
